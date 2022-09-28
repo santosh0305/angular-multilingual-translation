12f8c5c6dd7f62ba8de8fe22a27953a508b12ac7
santosh0305 <32795188+santosh0305@users.noreply.github.com>
2022-09-28 17:27:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-multilingual-translation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-multilingual-translation-qtpvqx)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }) ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
